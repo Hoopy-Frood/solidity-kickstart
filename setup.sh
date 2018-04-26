408bf44806fe7ae3776ee29cf9f185468572651e
Hoopy-Frood <andrew.cw.chan@gmail.com>
2018-04-26 22:09:12 +0800

    7.168 update

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 npm install --save ganache-cli mocha solc fs-extra web3@1.0.0-beta.26
 
 npm install --save next@4.1.4 react react-dom
+
+npm install --save semantic-ui-react
+
+npm install --save semantic-ui-css
