cb2bc3bc9a65e75b826d475f9d4433fd216d2353
Hoopy-Frood <andrew.cw.chan@gmail.com>
2018-04-26 23:45:50 +0800

    7.171 completed new.js except routing

--- pages/campaign/new.js
+++ pages/campaign/new.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
-import Layout from'../../components/Layout';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: ''
-    }
+        minimumContribution: '',
+        errorMessage: '',
+        loading: false
+    };
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+
+        this.setState({ errorMessage: '', loading: true });
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (err) {
+            this.setState({ errorMessage: err.message});
+        }
+        this.setState({ loading: false });
+    };
 
     render() {
         return (
             <Layout>
                 <h3>Create a Campaign</h3>
 
-                <Form>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input 
@@ -23,7 +44,8 @@ class CampaignNew extends Component {
                         />
                     </Form.Field>
 
-                    <Button primary>Create!</Button>
+                    <Message error header="Oops!" content={this.state.errorMessage} />
+                    <Button loading={this.state.loading} primary>Create!</Button>
                 </Form>
 
             </Layout>
